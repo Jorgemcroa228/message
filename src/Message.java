6f32218330c1cd85b3e29323e18adb9aeec50931
jorgemcroa228 <jorgemcroa228@gmail.com>
2026-02-26 00:12:45 -0500

    Everything is working

--- src/Message.java
+++ src/Message.java
@@ -2,11 +2,12 @@ public class Message {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello everyone!!!");
 
-        System.out.println("That me again");
+
     }
 
     public static void newMessage() {
         System.out.println("This is the second message");
-    }
 
+        System.out.println("It's me for a second time");
+    }
 }
