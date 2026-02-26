9041b78b611bf0653d8b9f2448594c6893f4cf14
jorgemcroa228 <jorgemcroa228@gmail.com>
2026-02-25 23:56:33 -0500

    Add aa new sout in main

--- src/Message.java
+++ src/Message.java
@@ -2,10 +2,11 @@ public class Message {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello everyone!!!");
 
-
+        System.out.println("That me again");
     }
 
     public static void newMessage() {
         System.out.println("This is the second message");
     }
+
 }
